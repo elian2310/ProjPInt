e4a4f52274d883af9b1362279f9495335ef75268
elian2310 <elianpacheco23.10@gmail.com>
2022-11-17 08:33:42 -0400

    Initial commit

---
+++ models/db_controller.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var con = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'nombre'
+});
+con.connect(function(err){
+    if(err){
+        throw err;
+    }else{
+        console.log('you are connected to database!');
+    }
+})
